83e895d5c2822f20b8cea51ffa8b434d04ede892
sungu0119 <52230781+sungu0119@users.noreply.github.com>
2019-06-27 17:52:07 +0900

    statfs

--- file/statfs.c
+++ file/statfs.c
@@ -8,7 +8,7 @@
 #include<stdlib.h>
 
 const char *MMOUNT = "/proc/mounts";
-
+//파일에 접근하려면 그에 앞서 파일을 포함하고 있는 파일 시스템을 mount 명령어를 이용하여 마운트 된 상태여야 한다 . sd 카드 usb 저장소 dbd 기타 이동식 저장소에 자주 사용된다
 struct f_size
 {
 		    long blocks;
@@ -42,13 +42,14 @@ MOUNTP *dfget(MOUNTP *MP)
 		    char buf[256];
 			char *bname;
 		    char null[16];
-			struct statfs lstatfs;
-			struct stat lstat; 
+			struct statfs lstatfs; //마운트 된 파일 시스템에 대한 정보를 반환하는 구조체
+			struct stat lstat; //파일의 상태를 확인할 수 있는 구조체
 			int is_root = 0;
 			// /proc/mounts로 부터 마운트된 파티션의 정보를 얻어온다.
 			while(fgets(buf, 255, MP->fp))
 			{
 					is_root = 0;
+					//sscanf 문자열로 함수가 데이터를 얻어옴
 					sscanf(buf, "%s%s%s",MP->devname, MP->mountdir, MP->fstype);
 					if (strcmp(MP->mountdir,"/") == 0) is_root=1;
 					if (stat(MP->devname, &lstat) == 0 || is_root)
@@ -63,6 +64,7 @@ MOUNTP *dfget(MOUNTP *MP)
 									}
 							}
 					}
+					//stream 과 연관된 파일 포인터를 파일 시작으로 이동한다.
 					rewind(MP->fp);
 					return NULL;
 }
