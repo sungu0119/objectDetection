6ee39685c2e993c6f4e5e1c473ee1078b06a8a09
sungu0119 <52230781+sungu0119@users.noreply.github.com>
2019-07-03 09:33:19 +0900

    https://wikidocs.net/22074

---
+++ qt5/qtime.py
@@ -0,0 +1,9 @@
+from PyQt5.QtCore import QTime,Qt
+
+time = QTime.currentTime()
+print(time.toString())
+print(time.toString('h.m.s'))
+print(time.toString('hh.mm.ss'))
+print(time.toString('hh.mm.ss.zzz'))
+print(time.toString(Qt.DefaultLocaleLongDate))
+print(time.toString(Qt.DefaultLocaleShortDate))
